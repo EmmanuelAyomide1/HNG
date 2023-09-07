0a53ae4243ce27035bb9d4db9970eb71f26176d7
unknown <owoeyeemmanuel206@gmail.com>
2023-09-07 17:43:21 +0100

    stage1 task

---
+++ stage1/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.http import JsonResponse
+# Create your views here.
+def Jsonresp(request):
+    slack_name = request.GET.get('slack_name')
+    track=request.GET.get('track')
+    day,utc_time=datetime.utcnow().strftime("%A %Y-%m-%DT:%M:%SZ").split()
+    return JsonResponse({"slack_name":slack_name,
+            "current_day":day,
+            "utc_time":utc_time,
+            "track":track,
+            "github_repo_url":"https://github.com/EmmanuelAyomide1/HNG",
+            "github_file_url":"t",
+            "status-code":200})
+
